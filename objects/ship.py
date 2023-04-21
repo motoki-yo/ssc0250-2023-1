c9ec0cea23bb12fe9459ea51625f0e2dd43275dc
jeff <jeffmbueno@duck.com>
2023-04-21 01:00:45 -0300

    Fix ship Mast + Sailors

--- objects/ship.py
+++ objects/ship.py
@@ -4,7 +4,7 @@ import numpy as np
 from OpenGL.GL import *
 
 class Ship():
-  data = np.zeros(360 + 4, [("position", np.float32, 2)])
+  data = np.zeros(180 + 6 + 3 + 3, [("position", np.float32, 2)])
   parts = []
   translateX = 0.0
   translateY = 0.0
@@ -12,7 +12,6 @@ class Ship():
 
   def __init__(self):
 
-    # Ship
     # Semi-ellipse
     for i in range(180):
         x = math.sin(math.radians(i+90)) * 0.6 # increase the radius along the x-axis
@@ -24,27 +23,42 @@ class Ship():
     )
 
     # Mast
-    # Thin rectangle
-    rect_width = 0.1
-    rect_height = 0.7
-    rect_x = -rect_width / 2
-    rect_y = rect_height # move the rectangle up, lower it to move it down
+    self.data[180] = [-0.05, 1] # Top left
+    self.data[181] = [0.05, 1] # Top right
+    self.data[182] = [-0.05, 0] # Bottom left
 
-    self.data[180] = [rect_x, rect_y]
-    self.data[181] = [rect_x + rect_width, rect_y]
-    self.data[182] = [rect_x + rect_width, rect_y - rect_height]
-    self.data[183] = [rect_x, rect_y - rect_height]
+    self.data[183] = [-0.05, 0] # Bottom left
+    self.data[184] = [0.05, 0] # Bottom right
+    self.data[185] = [0.05, 1] # Top right
 
     self.parts.append(
-    (GL_QUADS, 180, 184, (150, 75, 0))
+    (GL_TRIANGLES, 180, 186, (150, 75, 0))
+    )
+
+    # Sail right
+    self.data[186] = [0.085, 1] # Top of mast
+    self.data[187] = [0.085, 0.1] # Base on mast
+    self.data[188] = [0.6, 0.1] # On air
+
+    self.parts.append(
+    (GL_TRIANGLES, 186, 189, (255, 255, 255))
+    )
+
+    # Sail left
+    self.data[189] = [-0.085, 0.7] # Top of mast
+    self.data[190] = [-0.085, 0.1] # Base on mast
+    self.data[191] = [-0.5, 0.1] # On air
+
+    self.parts.append(
+    (GL_TRIANGLES, 189, 192, (255, 255, 255))
     )
 
     # Scaling and translating each vertex
     for i in range(0, len(self.data)):
       for vertex in self.data[i]:
         vertex *= .3
-      
-      self.data[i][0][1] -= 0.5
+
+      #self.data[i][0][1] -= 0.5
       
   def getTransformMatrix(self):
     translateMx = np.array([
